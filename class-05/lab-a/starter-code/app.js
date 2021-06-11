3ec26a06393c979fe2fe208ed356a64aeab489da
walaamohammad <layan_abadi86@yahoo.com>
2021-06-11 16:15:28 +0300

    problem3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -16,7 +16,7 @@ function sum(a, b) {
 // console.log(sum(4, 11));
 
 // Here is the test for sum(); uncomment it to run it
-   testSum();
+testSum();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -33,7 +33,7 @@ function multiply(a, b) { //eslint-disable-line
     let mul = a * b;
     return [mul, `The product of ${a} and ${b} is ${mul}.`];
 
-} 
+}
 
 // // Here is the test for multiply(); uncomment it to run it
 testMultiply();
@@ -52,12 +52,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the function and see if the test passes.*/
 
 // Write your code here
-// function sumAndMultiply(a, b, c) { //eslint-disable-line
-//   let sum = a + b + c;
-//   let total = a * b * c;
-//   return [sum, total, `${a} and ${b} and ${c}sum to ${sum}.` , `the multiple of ${a} and ${b} and ${c} is ${total}.`];
-// }
-// testSumAndMultiply();
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let sum1 = sum(a, b)[0];
+    let sum2 = sum(sum1, c)[0];
+    let multiply1 = multiply(a, b)[0];
+    let multiply2 = multiply(multiply1, c)[0];
+    return [sum2, multiply2, `${a} and ${b} and ${c} sum to ${sum2}.`, `The product of ${a} and ${b} and ${c} is ${multiply2}.`];
+}
+    testSumAndMultiply();
 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
